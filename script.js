de747ed093f223c3c96414879b4f361acab942ea
cloges4 <c4webdev@gmail.com>
2019-06-26 21:32:45 -0400

    added if statements to put leading zeros on minutes and seconds if less than 10

--- script.js
+++ script.js
@@ -4,12 +4,29 @@ const second = document.getElementById("second");
 
 const digitalClock = () => {
   const currentDate = new Date();
+  const currentMinute = currentDate.getMinutes();
+  const currentSecond = currentDate.getSeconds();
 
   hour.textContent = currentDate.getHours();
-  minute.textContent = currentDate.getMinutes();
-  second.textContent = currentDate.getSeconds();
+
+  // add a leading zero to minute of single digit
+  if (currentMinute < 10) {
+    minute.textContent = "0" + currentMinute;
+  } else {
+    minute.textContent = currentMinute;
+  }
+
+  // add a leading zero to second of single digit
+  if (currentSecond < 10) {
+    second.textContent = "0" + currentSecond;
+  } else {
+    second.textContent = currentSecond;
+  }
 };
 
 setInterval(digitalClock, 1000);
 
 digitalClock();
+
+//TODO  Fix clock box so that it does not change size based off number sizes
+//TODO finish styling page to look Good
